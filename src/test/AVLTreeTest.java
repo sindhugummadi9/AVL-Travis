9d4d9fa957e9ba8fdeffa178173775fcd39f5ab6
Sindhu Gummadi <sgummadi@cougarnet.uh.edu>
2022-04-09 11:48:42 -0500

    Travis commit

--- src/test/AVLTreeTest.java
+++ src/test/AVLTreeTest.java
@@ -1,4 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-class AVLTreeTest {
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AVLTreeTest {
+
+  private AVLTree avlTree;
+
+  @BeforeEach
+  void setup(){
+    avlTree = new AVLTree();
+  }
+
+  @Test
+  public void testFindWithOneElement(){
+    avlTree.insert(2);
+    AVLTree.Node node = avlTree.find(2);
+    assertEquals(2, node.key);
+  }
+
+  @Test
+  public void testFindWithTwoElements(){
+    avlTree.insert(2);
+    avlTree.insert(3);
+    AVLTree.Node node = avlTree.find(3);
+    assertEquals(3, node.key);
+  }
 
 }
