6f4a2e9462c918920860ee4cba52cad7469d5b7f
Sindhu Gummadi <sgummadi@cougarnet.uh.edu>
2022-04-16 22:53:56 -0500

    Bughunt - refactoring

--- src/test/java/avlTree/AVLTreeTest.java
+++ src/test/java/avlTree/AVLTreeTest.java
@@ -140,13 +140,13 @@ public class AVLTreeTest {
   @Test
   public void verifyDelete_whereLeftNodeIsNotNullButRightNodeIsNull_failingTestCase(){
     AVLTree avl = new AVLTree();
-    avl.insert(5);
-    avl.insert(3);
-    avl.insert(8);
-    avl.insert(6);
+    avl.insert(18);
+    avl.insert(13);
+    avl.insert(20);
+    avl.insert(19);
 
-    avl.delete(8);
-    assertNull(avl.find(8));
+    avl.delete(20);
+    assertNull(avl.find(20));
   }
 
 }
